59ccfd0287e7af88d0ceeda168a69a09181cf01b
MarharytaPusta <marharitapustaa@gmail.com>
2025-10-08 22:56:09 +0300

    refactoring

--- task.py
+++ task.py
@@ -9,54 +9,43 @@ class Valute_to_price():
         self.list_prices_to_sell = list_prices_to_sell
         self.list_prices_to_buy = list_prices_to_buy
 
-    def creation_valutes(self, driver, link, list_elem_lv_elem_lp):
+    def creation_valutes(self, driver, link, dict_elems):
         if self.list_valutes is None:
             self.list_valutes = []
         driver.get(link)
-        elem = driver.find_element(By.CLASS_NAME, list_elem_lv_elem_lp[0])
-        self.list_valutes = elem.find_elements(By.CSS_SELECTOR, list_elem_lv_elem_lp[1])
+        elem = driver.find_element(By.CSS_SELECTOR, dict_elems["general"])
+        self.list_valutes = elem.find_elements(By.CSS_SELECTOR, dict_elems["valute_selector"])
         self.list_valutes = [val.text for val in self.list_valutes]
 
-    def creation_sell(self, driver, link, list_elem_lv_elem_lp):
+    def creation_sell(self, driver, link, dict_elems):
         if self.list_prices_to_sell is None:
             self.list_prices_to_sell = []
         driver.get(link)
-        elem = driver.find_element(By.CLASS_NAME, list_elem_lv_elem_lp[2])
-        self.list_prices_to_sell = elem.find_elements(By.CSS_SELECTOR, list_elem_lv_elem_lp[3])
+        elem = driver.find_element(By.CSS_SELECTOR, dict_elems["general"])
+        self.list_prices_to_sell = elem.find_elements(By.CSS_SELECTOR, dict_elems["sell_selector"])
         self.list_prices_to_sell = [val.text for val in self.list_prices_to_sell]
-        print(len(self.list_prices_to_sell))
-        print(self.list_prices_to_sell)
-        print(len(self.list_valutes))
-        len_lv = len(self.list_valutes) * 3
-        if(len(self.list_prices_to_sell) > len(self.list_valutes) * 3):
-            self.list_prices_to_sell = self.list_prices_to_sell[: len_lv :]
-            print(self.list_prices_to_sell)
-        if(len(self.list_prices_to_sell) == len(self.list_valutes) * 3):
-            new_elems = []
-            for i in range(len(self.list_prices_to_sell)):
-                if i % 3 == 0:
-                    new_elems.append(self.list_prices_to_sell[i])
-            self.list_prices_to_sell = new_elems
-            print(self.list_prices_to_sell)
 
 
-    # def creation_buy(self, driver, link, list_elem_lv_elem_lp):
-    #     if self.list_prices_to_buy is None:
-    #         self.list_prices_to_buy = []
-    #     driver.get(link)
-    #     elem = driver.find_element(By.CLASS_NAME, list_elem_lv_elem_lp[4])
-    #     self.list_prices_to_sell = elem.find_elements(By.CSS_SELECTOR, list_elem_lv_elem_lp[5])
-    #     #self.print_values(driver)
+    def creation_buy(self, driver, link, dict_elems):
+        if self.list_prices_to_buy is None:
+            self.list_prices_to_buy = []
+        driver.get(link)
+        elem = driver.find_element(By.CSS_SELECTOR, dict_elems["general"])
+        self.list_prices_to_buy = elem.find_elements(By.CSS_SELECTOR, dict_elems["buy_selector"])
+        self.list_prices_to_buy = [val.text for val in self.list_prices_to_buy]
+        print("----------------------")
+        print(self.list_prices_to_buy)
+        print("----------------------")
 
 
     def print_values(self):
         for i in range(len(self.list_valutes)):
-            print(f"{self.list_valutes[i]} : {self.list_prices_to_sell[i]}")
+            print(f"{self.list_valutes[i]} : {self.list_prices_to_sell[i]} / {self.list_prices_to_buy[i]}")
 
-    def combine(self, driver, link, list_elem_lv_elem_lp):
-        self.creation_valutes(driver, link, list_elem_lv_elem_lp)
-        self.creation_sell(driver, link, list_elem_lv_elem_lp)
-        #self.creation_buy(driver, link, list_elem_lv)
+    def combine(self, driver, link, dict_elems):
+        self.creation_valutes(driver, link, dict_elems)
+        self.creation_sell(driver, link, dict_elems)
+        self.creation_buy(driver, link, dict_elems)
         self.print_values()
         driver.close()
 
@@ -64,8 +53,8 @@ class Valute_to_price():
 driver = webdriver.Chrome()
 # driver.implicitly_wait(5)
 money24 = Valute_to_price()
-list_elem_lv_elem_lp = ["map__courses-list", ".currency-node-wrapper span", "map__courses-list", ".map__courses-list-li div"]
-money24.combine(driver, "https://money24.com.ua/", list_elem_lv_elem_lp)
+dict_elems = {"general" : ".map__courses-list", "valute_selector" :  ".currency-node-wrapper span", "sell_selector" : "li div:first-child", "buy_selector" : "li div:last-child"}
+money24.combine(driver, "https://money24.com.ua/", dict_elems)
 
 
 
